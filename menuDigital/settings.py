d748110107ac25fc4de0cdac2c032bc9349b78fc
cribali <cristianbalihaut@gmail.com>
2024-09-02 17:39:34 -0300

    estilos boostrap a vistas del cliente

--- menuDigital/settings.py
+++ menuDigital/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'carta', # referencio la carta
+    'carta',
     'users'
     ]
 
--- users/views/cocina.py
+++ users/views/cocina.py
@@ -1,18 +1,23 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from carta.models import Cliente
+from django.contrib import messages
 
 
-class CocinaView(TemplateView):
+class CocinaView(LoginRequiredMixin,TemplateView):
     template_name = 'cocina.html'
+    login_url = '/users/login/'
+    redirect_field_name = 'next'
 
- 
     def get_context_data(self, **kwargs):
+       
         context = super().get_context_data(**kwargs)
         # Obtiene todos los clientes
         clientes = Cliente.objects.all()
         # Filtra clientes que tienen pedidos en los estados pendientes
         clientes_con_pedidos_pendientes = Cliente.objects.filter(
             pedido__estado__in=['confirmado', 'preparacion', 'listo_para_entregar']
-        ).distinct()
+        ).distinct() 
         context['clientes_con_pedidos_pendientes'] = clientes_con_pedidos_pendientes #diccionario context[clave] = valor
-        return context
+        return context 
